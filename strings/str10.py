7b1b46ccc18361735e181086732b54a5c9e05391
Priya Dsilva <priyadsilva@Priyas-MacBook-Pro.local>
2022-08-09 23:58:00 +0530

    str excercises

--- strings/str10.py
+++
@@ -1,5 +0,0 @@
-'''Write a Python program to change a given string to a new string where the first and last chars have been exchanged.'''
-
-string1 = "Python"
-
-print(string1[-1:] + string1[1:-1] + string1[:1])
